b514c7c7b66390c7d5bc496e3256199f939102c6
ShahirWardak <shahir.wardak@rspb.org.uk>
2024-11-30 20:50:05 +0000

    Added tamagui library

--- Scanner-app/app/_layout.tsx
+++ Scanner-app/app/_layout.tsx
@@ -1,5 +1,26 @@
 import { Stack } from "expo-router";
+import { TamaguiProvider } from "tamagui";
+import { useColorScheme } from "react-native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import config from "@/tamagui.config";
+
+//import config from "../tamagui.config";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+
+  return (
+    <TamaguiProvider config={config} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
+  );
 }
--- Scanner-app/app/index.tsx
+++ Scanner-app/app/index.tsx
@@ -1,6 +1,14 @@
-import { Button, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { Activity } from "@tamagui/lucide-icons";
+import { Text, View } from "react-native";
+import { Button } from "tamagui";
+import { TestComponent } from "@/components/test.component";
+import { useState } from "react";
 
 export default function Index() {
+  const [test, setTest] = useState(false);
+  const testText = "Hello!";
+
   return (
     <View
       style={{
@@ -9,8 +17,46 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Button title="Button title"></Button>
+      <Text style={styles.heading}>Welcome!</Text>
+
+      <Text style={styles.description}>
+        Edit app/index.tsx to edit this screen.
+      </Text>
+
+      <Button
+        themeInverse
+        iconAfter={Activity}
+        size="$3"
+        onPress={() => {
+          setTest(!test);
+        }}
+      >
+        Button!
+      </Button>
+
+      <Text style={styles.testStyle}>{test.toString()}</Text>
+      <Text>{testText}</Text>
+
+      <TestComponent></TestComponent>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  heading: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 6,
+    marginBottom: 50,
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  description: {
+    marginBottom: 20,
+  },
+  testStyle: {
+    flexDirection: "row",
+    alignItems: "center",
+    fontSize: 20,
+  },
+});
