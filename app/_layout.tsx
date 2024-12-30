6d0082ab7d5da6f64f36b062e4f6e9bd1b683d3c
ShahirWardak <shahir.wardak@rspb.org.uk>
2024-12-30 15:50:13 +0000

    index page and status bar styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,24 @@
 import { Stack } from "expo-router";
-import { TamaguiProvider } from "tamagui";
+import { TamaguiProvider, useThemeName } from "tamagui";
 import { useColorScheme } from "react-native";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
 import config from "@/tamagui.config";
 
 //import config from "../tamagui.config";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const themeName = useThemeName();
 
   return (
     <TamaguiProvider config={config} defaultTheme={colorScheme!}>
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <StatusBar style="auto" />
         <Stack
           screenOptions={{
             headerShown: false,
