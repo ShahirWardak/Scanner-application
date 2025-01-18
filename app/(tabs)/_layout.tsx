093a38c01022a6b95f7c6b4f935e57251bd7a41f
ShahirWardak <shahir.wardak@rspb.org.uk>
2025-01-18 16:05:13 +0000

    tab bar styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,89 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { StyleSheet } from "react-native";
 import { Tabs } from "expo-router";
+import { ScanBarcode, Search, ShoppingBasket } from "@tamagui/lucide-icons";
+import { useThemeName, View } from "tamagui";
 
 export default function TabLayout() {
+  const themeName = useThemeName();
+
   return (
     <Tabs
       backBehavior="firstRoute"
       screenOptions={{
-        tabBarActiveTintColor: "blue",
+        tabBarActiveTintColor: "white",
         headerShown: false,
         animation: "shift",
+        tabBarShowLabel: false,
+        tabBarLabelPosition: "beside-icon",
+        tabBarStyle: styles.tabBarStyles,
+        tabBarItemStyle: styles.tabItemStyles,
+        tabBarIconStyle: styles.tabIconStyles,
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="home" color={color} />
-          ),
+          title: "Items",
+          tabBarIcon: ({ color, focused }) =>
+            focused ? (
+              <View style={styles.iconWrapperStyles} backgroundColor="green">
+                <ShoppingBasket size="$2" color={color} />
+              </View>
+            ) : (
+              <ShoppingBasket size="$2" color={color} />
+            ),
         }}
       />
       <Tabs.Screen
         name="scanner"
         options={{
-          title: "Scanner",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="camera-retro" color={color} />
-          ),
+          title: "Scan",
+          tabBarIcon: ({ color, focused }) =>
+            focused ? (
+              <View style={styles.iconWrapperStyles} backgroundColor="green">
+                <ScanBarcode size="$2" color={color} />
+              </View>
+            ) : (
+              <ScanBarcode size="$2" color={color} />
+            ),
         }}
       />
       <Tabs.Screen
         name="item-search"
         options={{
           title: "Search",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="search" color={color} />
-          ),
+          tabBarIcon: ({ color, focused }) =>
+            focused ? (
+              <View style={styles.iconWrapperStyles} backgroundColor="green">
+                <Search size="$2" color={color} />
+              </View>
+            ) : (
+              <Search size="$2" color={color} />
+            ),
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBarStyles: {
+    marginBottom: 20,
+    marginRight: 10,
+    marginLeft: 10,
+    borderRadius: 50,
+    elevation: 4,
+    height: 60,
+  },
+  tabItemStyles: {
+    //justifyContent: "center",
+  },
+  tabIconStyles: {
+    //justifyContent: "center",
+  },
+  iconWrapperStyles: {
+    padding: 10,
+    paddingHorizontal: 40,
+    borderRadius: 50,
+  },
+});
